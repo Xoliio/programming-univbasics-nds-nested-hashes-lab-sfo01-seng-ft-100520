accb70dfd80b55f872b53d4aeeec6b76eb3f1a21
Inee Ader <xoliio@gmail.com>
2020-09-03 18:09:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,6 +7,13 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
+  BIG_ARRAY = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+  ]
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 end
@@ -17,6 +24,7 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+
 end
 
 def aoh_update(aoh, row, key, new_value)
