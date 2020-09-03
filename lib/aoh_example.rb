5b17f3c16343ed672e6cbe67e438dac8c850751b
Inee Ader <xoliio@gmail.com>
2020-09-03 18:53:21 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,7 @@ def assembled_aoh
   ]
 end
 
-def literal_aoh (constant, key, value)
+def literal_aoh (hash, key)
   new_array = [
     DON_G[:name, :occupation], JOELLE_VD[:name, :occupation], PAT_M[:name, :occupation], KATE_G[:name, :occupation], BRUCE_G[:name, :occupation]
   ]
